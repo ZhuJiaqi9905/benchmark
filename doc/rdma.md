bd26aee868f38a2a736263f78f819160e81bacf5
ZhuJiaqi <zhu_jiaqi@pku.edu.cn>
2022-01-11 20:21:08 +0800

    feat(src/rdma/read): rdma seq read benchmark

--- doc/rdma.md
+++ doc/rdma.md
@@ -4,8 +4,17 @@
 
 server把数据从文件中读取到内存。client端生成一个等大的buffer，通过rdma read操作，每次只能读取batch_size大小的数据。client端一直读取，直到把数据读完。
 
-- 设置qp的sq_sig_all为0，使得大部分read request不产生cqe。
-- 记录wqe的个数，当cq要满了时，放置一个产生cqe的wqe。
+* 设置qp的sq_sig_all为0，使得大部分read request不产生cqe。
+* 记录wqe的个数，当cq要满了时，放置一个产生cqe的wqe。
 
-batch_size | throughput
+batch_size(B) | throughput(MB/s)
 ----- | -----
+1024 | 409.291
+10240 | 2339.829
+102400 | 5286.479
+204800 | 5786.241
+1024000 | 6365.728
+4096000 | 6474.686
+10240000 | 6363.948
+
+可以看到RDMA read带宽就是6GB/s左右了，而且这种read是远端准备好自己的数据，然后本地直接read数据，才能达到这样的吞吐量。如果每次仅读取小数据，是无法跑满带宽的。
--- src/main.rs
+++ src/main.rs
@@ -12,19 +12,19 @@ mod serial;
 struct Args {
     #[clap(long)]
     bench: String,
-    #[clap(long)]
+    #[clap(long, default_value = "")]
     rdma: String,
 }
 fn main() {
     let args = Args::parse();
     if args.bench == "disk" {
-        read_throughput("data/bigfile.log", 5120);
-        bufread_throughput("data/bigfile.log", 5120);
+        read_throughput("data/bigfile.log", 4096000);
+        bufread_throughput("data/bigfile.log", 4096000);
     } else if args.bench == "serial" {
         test_serialize("data/bigfile.log", 1024);
     } else if args.bench == "rdma" {
         if args.rdma == "read_server" {
-            test_rserver("10.0.12.24:9500", "data/smallfile.txt");
+            test_rserver("10.0.12.24:9500", "data/bigfile.log");
         } else if args.rdma == "read_client" {
             test_rclient("10.0.12.24:9500");
         }
