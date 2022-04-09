b73f93265944b4cb5654f897b760fd7931753cbe
zhujiaqi <zhu_jiaqi@pku.edu.cn>
2022-04-09 14:04:47 +0800

    update script

--- script/read_client.sh
+++ script/read_client.sh
@@ -1,2 +1,2 @@
 #!/bin/bash
-cargo run --release --example read_client -- -a 127.0.0.1:9900 -s 1024 -m 1024 
+cargo run --release --example read_client -- -a 10.0.12.24:9900 -s 1073741824 -m 1048576 
--- script/read_server.sh
+++ script/read_server.sh
@@ -1,2 +1,2 @@
 #!/bin/bash
-cargo run --release --example read_server -- -a 127.0.0.1:9900 -s 1024 
+cargo run --release --example read_server -- -a 127.0.0.1:9900 -s 1073741824
